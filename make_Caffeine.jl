feaf3e9c9f191139aabd1818054577d1107e3fbf
Michael F. Herbst <info@michael-herbst.com>
2020-10-27 08:11:40 +0100

    make_Caffeine update

--- make_Caffeine.jl
+++ make_Caffeine.jl
@@ -1,4 +1,5 @@
 using DFTK
+using JLD2
 
 Ecut = 35
 
@@ -11,6 +12,6 @@ basis = PlaneWaveBasis(model, Ecut, kgrid=[1, 1, 1])
 scfres = self_consistent_field(basis; tol=100, maxiter=1, mixing=SimpleMixing(0.0))
 
 # Truncate scfres such that density is guess density
-scfres = (ρ=guess_density(basis), ρspin=nothing,basis=basis, ψ=scfres.ψ,
+scfres = (ρ=guess_density(basis), ρspin=nothing, basis=basis, ψ=scfres.ψ,
           occupation=scfres.occupation, eigenvalues=scfres.eigenvalues, εF=scfres.εF)
 save_scfres("Caffeine_scfres_guess.jld2", scfres)
