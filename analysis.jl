97163de4829bd5cc3bde4268c181068ebb5e7757
Michael F. Herbst <info@michael-herbst.com>
2020-11-05 21:07:38 +0100

    Improve display

--- analysis.jl
+++ analysis.jl
@@ -6,16 +6,16 @@ function plot_all_cases(df, basetime, x, y=:time; caseminima=Dict(), ideal=ident
     p = plot(;xlabel=string(x), ylabel="relative $y", legend=:topright, kwargs...)
 
     for (i, case) in enumerate(cases)
-        minimum = get(caseminima, case, nothing) ./ basetime[case]
+        minimum = basetime[case] ./ get(caseminima, case, nothing)
         xvals = getproperty(df[df.case .== case, :], x)
         yvals = getproperty(df[df.case .== case, :], y)
-        plot!(p, xvals, yvals ./ basetime[case], label=case, color=i)
+        plot!(p, xvals, basetime[case] ./ yvals, label=case, color=i, mark=:x)
         if !isnothing(minimum)
             plot!(p, xvals, minimum * ones(Float64, size(yvals)), color=i, label="", linestyle=:dot)
         end
     end
     xvals = getproperty(df[df.case .== "Fe", :], x)
-    plot!(p, xvals, 1 ./ ideal.(xvals), label="perfect scaling", colour=:black, linestyle=:dash)
+    plot!(p, xvals, ideal.(xvals), label="perfect scaling", colour=:black, linestyle=:dash)
     p
 end
 
@@ -25,37 +25,40 @@ function find_caseminima(df)
 end
 function find_basetime(df)
     cases = unique(df[.!ismissing.(df.time), :].case)
-    Dict(case => only(df[(df.case .== case) .& (df.n_fft .== df.n_julia .== df.n_blas .== 1), :].time)
+    Dict(case => only(df[(df.case .== case) .& (df.n_fft .== df.n_julia .== df.n_blas .== df.n_mpi .== 1), :].time)
          for case in cases)
 end
 
 df = collect_dataframe()
 df = df[(df.case .!= "Si"), :]
 
+ylims = (1, 10)
+
 Plots.default(titlefontsize=10, guidefontsize=8, legendfontsize=6, tickfontsize=6)
 caseminima = find_caseminima(df)
 basetime   = find_basetime(df)
-p1 = plot_all_cases(df[(df.n_julia .== 1) .& (df.n_fft .== 1),  :], basetime, :n_blas;
-                    title="n_blas series", caseminima=caseminima, legend=:bottomleft)
-p2 = plot_all_cases(df[(df.n_julia .== 1) .& (df.n_blas .== 1), :], basetime, :n_fft;
-                    title="n_fft series", caseminima=caseminima, legend=:none)
-p3 = plot_all_cases(df[(df.n_blas .== 1)  .& (df.n_fft .== 1),  :], basetime, :n_julia;
-                    title="n_julia series", caseminima=caseminima, legend=:none)
-p = plot(p1, p2, p3)
+p1 = plot_all_cases(df[(df.n_julia .== 1) .& (df.n_fft .== 1) .& (df.n_mpi .== 1),  :],
+                    basetime, :n_blas; title="n_blas series", caseminima=caseminima, legend=:topleft, ylims=ylims)
+p2 = plot_all_cases(df[(df.n_julia .== 1) .& (df.n_blas .== 1) .& (df.n_mpi .== 1), :],
+                    basetime, :n_fft; title="n_fft series", caseminima=caseminima, legend=:none, ylims=ylims)
+p3 = plot_all_cases(df[(df.n_blas .== 1)  .& (df.n_fft .== 1) .& (df.n_mpi .== 1),  :],
+                    basetime, :n_julia; title="n_julia series", caseminima=caseminima, legend=:none, ylims=ylims)
+p4 = plot_all_cases(df[(df.n_blas .== 1)  .& (df.n_fft .== 1) .& (df.n_julia .== 1),  :],
+                    basetime, :n_mpi; title="n_mpi series", caseminima=caseminima, legend=:none, ylims=ylims)
+p = plot(p1, p2, p3, p4)
 savefig(p, "thread_axes.pdf")
 
-ylims = (0.0, 1.0)
-q1 = plot_all_cases(df[(df.n_blas .== df.n_julia)  .& (df.n_fft .== 1),  :], basetime, :n_julia;
-                    title="n_julia = n_blas, n_fft = 1", ylims=ylims,
-                    caseminima=caseminima, legend=:topright)
-q2 = plot_all_cases(df[(df.n_blas .== df.n_julia)  .& (df.n_fft .== 2),  :], basetime, :n_julia;
-                    title="n_julia = n_blas, n_fft = 2", ylims=ylims, ideal=x -> 2x,
-                    caseminima=caseminima, legend=:none)
-q3 = plot_all_cases(df[(df.n_blas .== min.(6, df.n_julia)) .& (df.n_fft .== 1),  :], basetime, :n_julia;
-                    title="n_blas = min(6, n_julia), n_fft = 1", ylims=ylims,
-                    caseminima=caseminima, legend=:none)
-q4 = plot_all_cases(df[(df.n_blas .== min.(6, df.n_julia)) .& (df.n_fft .== 2),  :], basetime, :n_julia;
-                    title="n_blas = min(6, n_julia), n_fft = 2", ylims=ylims,
+q1 = plot_all_cases(df[(df.n_blas .== df.n_julia)  .& (df.n_fft .== df.n_mpi .== 1),  :],
+                    basetime, :n_julia; title="n_julia = n_blas, n_fft = 1", ylims=ylims,
+                    caseminima=caseminima, legend=:topleft)
+q2 = plot_all_cases(df[(df.n_blas .== df.n_julia)  .& (df.n_fft .== 2) .& (df.n_mpi .== 1),  :],
+                    basetime, :n_julia; title="n_julia = n_blas, n_fft = 2", ylims=ylims,
+                    ideal=x -> 2x, caseminima=caseminima, legend=:none)
+q3 = plot_all_cases(df[(df.n_blas .== min.(6, df.n_julia)) .& (df.n_fft .== df.n_mpi .== 1),  :],
+                    basetime, :n_julia; title="n_blas = min(6, n_julia), n_fft = 1",
+                    ylims=ylims, caseminima=caseminima, legend=:none)
+q4 = plot_all_cases(df[(df.n_blas .== min.(6, df.n_julia)) .& (df.n_fft .== 2) .& (df.n_mpi .== 1),  :],
+                    basetime, :n_julia; title="n_blas = min(6, n_julia), n_fft = 2", ylims=ylims,
                     caseminima=caseminima, legend=:none, ideal=x-> 2x)
 q = plot(q1, q2, q3, q4)
 savefig(q, "thread_settings.pdf")
